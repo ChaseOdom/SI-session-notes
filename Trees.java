35f316b7b2a68978ac3bf3886f51be50749c9492
ChaseOdom <35878548+ChaseOdom@users.noreply.github.com>
2019-04-08 16:27:11 -0500

    Add files via upload

---
+++ Trees.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Trees  {
+	public static void main(String[] args) throws FileNotFoundException{
+		File t = new File("trees.txt");
+		Scanner fileScan = new Scanner(t);
+		
+		//coding here kudasai
+		
+		fileScan.close();
+	}
+	
+	//and here too
+}
