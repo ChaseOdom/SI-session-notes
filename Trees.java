0bc4c7673fd46b0a5cc5ff83adddede79447793e
ChaseOdom <35878548+ChaseOdom@users.noreply.github.com>
2019-04-22 17:33:53 -0500

    Add files via upload

--- Trees.java
+++ Trees.java
@@ -1,16 +1,42 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Trees  {
+	
+	static char[][] grid1 = {
+			{'1','2','3'},
+			{'4','5','6'},
+			{'7','8','9'}
+	};
+	
+	static char[][] grid2 = {
+			{'c','c','3'},
+			{'4','c','6'}
+	};
+	
+	static char[][] grid3 = {
+			{'c','c'},
+			{'4','c'},
+			{'1','d'}
+	};
+	
 	public static void main(String[] args) throws FileNotFoundException{
-		File t = new File("trees.txt");
-		Scanner fileScan = new Scanner(t);
-		
-		//coding here kudasai
-		
-		fileScan.close();
+		charCount(grid1);
+		raiseC(grid2);
+		topMirror(grid3);
 	}
 	
-	//and here too
+	public static void charCount(char[][] sqrGrid){
+		
+	}
+	
+	public static void raiseC(char[][] grid){
+		
+	}
+	
+	public static void topMirror(char[][] grid){
+	
+	}
 }
