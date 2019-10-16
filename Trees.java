4e4fa9d0c163af4910f717cfbb5643ba0a65fb68
ChaseOdom <35878548+ChaseOdom@users.noreply.github.com>
2019-10-16 15:58:46 -0500

    Update Trees.java

--- Trees.java
+++ Trees.java
@@ -5,38 +5,7 @@ import java.util.Scanner;
 
 public class Trees  {
 	
-	static char[][] grid1 = {
-			{'1','2','3'},
-			{'4','5','6'},
-			{'7','8','9'}
-	};
-	
-	static char[][] grid2 = {
-			{'c','c','3'},
-			{'4','c','6'}
-	};
-	
-	static char[][] grid3 = {
-			{'c','c'},
-			{'4','c'},
-			{'1','d'}
-	};
-	
 	public static void main(String[] args) throws FileNotFoundException{
-		charCount(grid1);
-		raiseC(grid2);
-		topMirror(grid3);
-	}
-	
-	public static void charCount(char[][] sqrGrid){
 		
-	}
-	
-	public static void raiseC(char[][] grid){
-		
-	}
-	
-	public static void topMirror(char[][] grid){
-	
 	}
 }
